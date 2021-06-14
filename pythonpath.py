a86c5503f6fa56a2d8bae89c8d7a4aed1b099731
Steven J. DeRose <sderose@docugami.com>
2021-06-14 13:48:30 -0400

    new layout, pythonpath.py

--- pythonpath.py
+++ pythonpath.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# pythonpath -- display and check where Python is going to look.
+# pythonpath.py: Display and check where Python is going to look.
 # 2014-06-12: Written by Steven J. DeRose.
 #
 from __future__ import print_function
@@ -8,24 +8,26 @@ import sys
 import os
 import re
 import argparse
-import subprocess
+from subprocess import check_output, CalledProcessError
 import glob
 
 from sjdUtils import sjdUtils
+su = sjdUtils()
+lg = su.getLogger()
 
 __metadata__ = {
-    'title'        : "pythonpath.py",
-    'description'  : "display and check where Python is going to look",
-    'rightsHolder' : "Steven J. DeRose",
-    'creator'      : "http://viaf.org/viaf/50334488",
-    'type'         : "http://purl.org/dc/dcmitype/Software",
-    'language'     : "Python 3.7",
-    'created'      : "2014-06-12",
-    'modified'     : "2020-11-06",
-    'publisher'    : "http://github.com/sderose",
-    'license'      : "https://creativecommons.org/licenses/by-sa/3.0/"
+    "title"        : "pythonpath.py",
+    "description"  : "Display and check where Python is going to look.",
+    "rightsHolder" : "Steven J. DeRose",
+    "creator"      : "http://viaf.org/viaf/50334488",
+    "type"         : "http://purl.org/dc/dcmitype/Software",
+    "language"     : "Python 3.7",
+    "created"      : "2014-06-12",
+    "modified"     : "2020-11-06",
+    "publisher"    : "http://github.com/sderose",
+    "license"      : "https://creativecommons.org/licenses/by-sa/3.0/"
 }
-__version__ = __metadata__['modified']
+__version__ = __metadata__["modified"]
 
 
 descr = """
@@ -48,11 +50,10 @@ of the same directory or class name.
 =To do=
 
 * CATCH existing .pyc when there's no .py in same dir.
-* Identify pip, port, brew, easy_install
-* Hook up getClassDefs2()!!! FIX
-* Check for file, or even classes, defined at more than one place on path
-* Check for more module conventions?
-* CF: findExternals.py.
+* Identify pip, port, brew, easy_install, etc.
+* Hook up getClassDefs2()!!! TODO
+* Check for files or classes, defined at more than one place on path
+* Cf: findExternals.py.
 
 
 =Rights=
@@ -68,17 +69,12 @@ For the most recent version, see [http://www.derose.net/steve/utilities] or
 =Options
 """
 
-args = None
-su = sjdUtils()
-lg = su.getLogger()
-
-
 def notify(level, msg, color=None):
     if (args.where): return
     if (color): lg.vMsg(level, msg, color=color)
     else: lg.vMsg(level, msg)
 
-def prob(level, msg, color='red'):
+def prob(level, msg, color="red"):
     if (args.where): return
     if (color): lg.vMsg(level, msg, color=color)
     else: lg.vMsg(level, msg)
@@ -88,17 +84,17 @@ def hasPythonModule(curDir):
     if (os.path.exists(os.path.join(curDir,"__init__.py"))): return(True)
     subs = os.listdir(curDir)
     for sub in (subs):
-        if (re.search(r'\.(py|pm|pyc)$', sub)): return(True)
+        if (re.search(r"\.(py|pm|pyc)$", sub)): return(True)
     return(False)
 
-def commonLen(a,b, onlyEndAt=''):
+def commonLen(a,b, onlyEndAt=""):
     """Find how much of the start of two strings is the same.
-    If 'wholeDirs' is set, only end at given character (e.g. '/')
+    If "wholeDirs" is set, only end at given character (e.g. "/")
     """
     n = min(len(a),len(b))
     for i in (range(0, n)):
         if (a[i] != b[i]):
-            if (onlyEndAt == ''): return(i-1)
+            if (onlyEndAt == ""): return(i-1)
             if (onlyEndAt not in a[0:i]): return(i-1)
             lastSlash = a[0:i].rfind(onlyEndAt)
             return(lastSlash)
@@ -107,20 +103,20 @@ def commonLen(a,b, onlyEndAt=''):
 
 def getClassDefs(path, recursive=False):
     """Extract all the class definitions from a given file.
-    For now, do this with 'grep'. Might be better to have Python actually
+    For now, do this with "grep". Might be better to have Python actually
     parse the file, and then examine what externals have been defined.
     """
-    cmdTokens = [ "grep", "-H", "'^class '" ]
+    cmdTokens = [ "grep", "-H", '"^class "' ]
     if (recursive): cmdTokens.insert(1, "-r")
-    flist = glob.glob(path+'/*.py')
+    flist = glob.glob(path+"/*.py")
     if (len(flist)<1): return("")
     cmdTokens.extend(flist)
     lg.vMsg(1, "getClassDefs cmd: %s" % ("\\\n    ".join(cmdTokens)))
     try:
-        buf = subprocess.check_output(cmdTokens, shell=False, stderr=None)
-        if (type(buf) == type('x')):
+        buf = check_output(cmdTokens, shell=False, stderr=None)
+        if (type(buf) == type("x")):
             bufRecs = buf.split(sep="\n")
-    except Exception as e:
+    except CalledProcessError as e:
         sys.stderr.write("Exception: %s" % (e))
         bufRecs = []
     return(bufRecs)
@@ -130,7 +126,7 @@ def getClassDefs2(path):
     Given a single file, find out what classes it defines.
     *** Should make a "list modules used, with paths" thing.
     Doc says that pyclbr.readmodule `pathList` arg "is used to augment
-    the value of sys.path". In fact it is checked ''before'' sys.path,
+    the value of sys.path". In fact it is checked ""before"" sys.path,
     so can be used to check a specific file.
     """
     import pyclbr
@@ -159,12 +155,12 @@ def getClassDefsOfWholeDir(curDir):
     *** Should make a "list modules use with paths" thing.
     pyclbr.readmodule(moduleName, pathList) does what I want.
     Doc says that `pathList` "is used to augment the value of sys.path". In fact
-    it is checked ''before'' sys.path, so can be used to check a specific file.
+    it is checked ""before"" sys.path, so can be used to check a specific file.
     """
     bigDict = {}
     for f in os.listdir(curDir):
         if (f.endswith(".pyc")):
-            lg.vMsg(0, ".pyc found: %s/%s" % (curDir,f))
+            lg.warning0(".pyc found: %s/%s" % (curDir,f))
             continue
         elif (not f.endswith(".py")):
             continue
@@ -184,14 +180,14 @@ def showClasses(curDir):
     lg.MsgPush()
     classesSeen = {}
     for cd in (cds):
-        if (cd==''): continue
+        if (cd==""): continue
         if (args.where):
             if (re.search(args.where, cd)):
-                lg.vMsg(0, "*** FOUND *** %s" % (cd))
+                lg.warning0("*** FOUND *** %s" % (cd))
         else:
-            lg.vMsg(0, "... %s" % (cd[len(curDir):]))
+            lg.warning0("... %s" % (cd[len(curDir):]))
         if (cd in classesSeen):
-            cl = re.sub(r'^.*class ','',cd)
+            cl = re.sub(r"^.*class ", "", cd)
             prob(0, "    -- Duplicate def of class '%s'" % cl)
             classesSeen[cd] += "\n\t" + curDir
         else:
@@ -203,77 +199,23 @@ def showClasses(curDir):
             print("%s:\n\t%s\n" % (k, v))
     return()
 
-
-###############################################################################
-# Process options
-#
-def processArgs():
-    try:
-        from BlockFormatter import BlockFormatter
-        parser = argparse.ArgumentParser(
-            description=descr, formatter_class=BlockFormatter)
-    except ImportError:
-        parser = argparse.ArgumentParser(description=descr)
-
-    parser.add_argument(
-        "--abbrevs",         action='store_true',
-        help='Leave out portion of each path that is the same as previous.')
-    parser.add_argument(
-        "--classes",         action='store_true',
-        help='Also report what classes are defined where.')
-    colorDefault = (sys.stdout.isatty() and "USE_COLOR" in os.environ)
-    parser.add_argument(
-        "--color",           action='store_true',   default=colorDefault,
-        help='Use color to improve readability.')
-    parser.add_argument(
-        "--extras", "-x",     action='store_true',
-        help='Report any paths in $PYTHONPATH that aren\'t in sys.path.')
-    parser.add_argument(
-        "--find",            type=str, metavar="T",
-        help='Do "find" for  T.py under the given directory.')
-    parser.add_argument(
-        "--init",            action='store_true',
-        help='Report any paths with no __init__.py file.')
-    parser.add_argument(
-        "--pathmods",       action='store_true',
-        help='Search for code that modifies sys.path.')
-    parser.add_argument(
-        "--quiet", "-q",     action='store_true',
-        help='Suppress most messages.')
-    parser.add_argument(
-        "--sys",             action='store_true',
-        help='Report sys.path as well as path from environment.')
-    parser.add_argument(
-        "--verbose", "-v",    action='count',       default=0,
-        help='Add more messages (repeatable).')
-    parser.add_argument(
-        '--version',          action='version', version=__metadata__['__version__'],
-        help='Display version information, then exit.')
-    parser.add_argument(
-        '--where',            type=str,             default="",
-        help='Search for a specific class or packge and report curDir(s).')
-
-    args0 = parser.parse_args()
-    return(args0)
-
-
 def checkPYTHONPATH():
     notify(0, "Checking PYTHONPATH:")
     pp = os.environ["PYTHONPATH"]
     if (not pp):
-        lg.warn(-1, "Can't find $PYTHONPATH.")
+        lg.fatal("Can't find $PYTHONPATH.")
 
-    dirs = re.split(r':', pp)
+    dirs = re.split(r":", pp)
     dirsSeen = {}
     classesSeen = {}
     lastD = ""
     for curDir in (dirs):
         if (args.classes): notify(0,"")
         if (args.abbrevs):
-            clen = commonLen(lastD, curDir, onlyEndAt='/')
+            clen = commonLen(lastD, curDir, onlyEndAt="/")
             printD = (" " * (clen+1)) + curDir[clen:]
         elif (args.color):
-            clen = commonLen(lastD, curDir, onlyEndAt='/')
+            clen = commonLen(lastD, curDir, onlyEndAt="/")
             printD = curDir[0:clen+1] + su.colorize("blue", curDir[clen:])
         else:
             printD = curDir
@@ -287,14 +229,14 @@ def checkPYTHONPATH():
             prob(0, "    Empty entry")
             continue
 
-        if (re.search(r'/\.\./|/\.|//|\$', curDir)):
+        if (re.search(r"/\.\./|/\.|//|\$", curDir)):
             prob(0, "    Path is not normalized")
         if (not os.path.isdir(curDir)):
-            prob(0, "    Directory does not exist", color='/red')
+            prob(0, "    Directory does not exist", color="/red")
             continue
         if (args.init and
-            not os.path.exists(os.path.join(curDir,"__init__.py"))):
-            prob(0, "    Directory does not contain __init__.py", color='/red')
+            not os.path.exists(os.path.join(curDir, "__init__.py"))):
+            prob(0, "    Directory does not contain __init__.py", color="/red")
         if (not hasPythonModule(curDir)):
             prob(0, "    No modules found here")
         if (args.classes):
@@ -309,12 +251,12 @@ def searchForFile(sysdirs, tgt):
     print("\nRunning finds for '%s'..." % (tgt))
     whereFound = []
     for curDir in sysdirs:
-        fcmd = [ 'find', curDir, '-name', tgt, '-print', '-maxdepth', '1' ]
+        fcmd = [ "find", curDir, "-name", tgt, "-print", "-maxdepth", "1" ]
         lg.vMsg(2, "Running: %s" % (" ".join(fcmd)))
         try:
-            out = subprocess.check_output(fcmd, stderr=toss)
+            out = check_output(fcmd, stderr=toss)
             if (out.strip()): whereFound.append(out.strip())
-        except subprocess.CalledProcessError:
+        except CalledProcessError:
             pass  # Nothing found here
     return(whereFound)
 
@@ -322,21 +264,70 @@ def searchForFile(sysdirs, tgt):
 ###############################################################################
 # Main
 #
+def processOptions():
+    try:
+        from BlockFormatter import BlockFormatter
+        parser = argparse.ArgumentParser(
+            description=descr, formatter_class=BlockFormatter)
+    except ImportError:
+        parser = argparse.ArgumentParser(description=descr)
+
+    parser.add_argument(
+        "--abbrevs", action="store_true",
+        help="Leave out portion of each path that is the same as previous.")
+    parser.add_argument(
+        "--classes", action="store_true",
+        help="Also report what classes are defined where.")
+    colorDefault = (sys.stdout.isatty() and "USE_COLOR" in os.environ)
+    parser.add_argument(
+        "--color", action="store_true", default=colorDefault,
+        help="Use color to improve readability.")
+    parser.add_argument(
+        "--extras", "-x", action="store_true",
+        help="Report any paths in $PYTHONPATH that aren't in sys.path.")
+    parser.add_argument(
+        "--find", type=str, metavar="T",
+        help="Do 'find' for T.py under the given directory.")
+    parser.add_argument(
+        "--init", action="store_true",
+        help="Report any paths with no __init__.py file.")
+    parser.add_argument(
+        "--pathmods", action="store_true",
+        help="Search for code that modifies sys.path.")
+    parser.add_argument(
+        "--quiet", "-q", action="store_true",
+        help="Suppress most messages.")
+    parser.add_argument(
+        "--sys", action="store_true",
+        help="Report sys.path as well as path from environment.")
+    parser.add_argument(
+        "--verbose", "-v", action="count", default=0,
+        help="Add more messages (repeatable).")
+    parser.add_argument(
+        "--version", action="version", version=__metadata__["__version__"],
+        help="Display version information, then exit.")
+    parser.add_argument(
+        "--where", type=str, default="",
+        help="Search for a specific class or packge and report curDir(s).")
+
+    args0 = parser.parse_args()
+    return(args0)
+
 classDefs = {}
-args = processArgs()
+args = processOptions()
 su.setVerbose(args.verbose)
 su.setColors(args.color)
 
 if (args.where): args.classes = True
 
 if ("PYTHONHOME" in os.environ):
-    lg.warn(0, "Warning: $PYTHONHOME is set, to " + os.environ["PYTHONHOME"])
+    lg.warning0("Warning: $PYTHONHOME is set: '%s'." % (os.environ["PYTHONHOME"]))
 
 dirs0, dirsSeen0, classesSeen0 = checkPYTHONPATH()
 syspath = sys.path
 
 if (args.sys or args.verbose):
-    lg.hMsg(0, "%d in sys.path (indented ones are also in $PYTHONPATH):" %
+    lg.warning0("%d in sys.path (indented ones are also in $PYTHONPATH):" %
         (len(syspath)))
     for sysDir in (syspath):
         if (sysDir in dirsSeen0): print("    "+sysDir)
@@ -345,21 +336,17 @@ if (args.sys or args.verbose):
 if (not (set(dirs0) < set(syspath))):
     prob(0, "\n$PYTHONPATH not a subset of sys.path. Extras:")
     xtra = set(dirs0)-set(syspath)
-    lg.vMsg(0, "\n    ".join(xtra))
+    lg.warning0("\n    ".join(xtra))
 
 if (args.find):
     whereFound0 = searchForFile(syspath, args.find)
     for wh in whereFound0:
-        print("    " + re.sub(r'\n', '\n    ', wh))
+        print("    " + re.sub(r"\n", "\n    ", wh))
 
 if (args.pathmods):
     print("\nSearching for code referring to sys.path.")
     for sysDir in (dirs0):
-        x = subprocess.check_output([ 'grep', '-r "sys\\.path"', sysDir],
+        x = check_output([ "grep", '-r "sys\\.path"', sysDir],
             stderr=None)
         if (x):
             print("\nUnder %s\n%x" % (sysDir,x))
-
-lg.vMsg(0, "Done.")
-
-sys.exit(0)
